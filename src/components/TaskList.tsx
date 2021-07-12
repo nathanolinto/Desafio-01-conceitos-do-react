ec45b45e2880efc1c2d6c9d172551bf61de00cb7
nathanolinto <nathanolinto@gmail.com>
2021-07-12 10:44:38 -0300

    Finalizando o desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -11,19 +11,36 @@ interface Task {
 }
 
 export function TaskList() {
+  //const [count, setCount] = useState(0);
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle){
+      return;
+    }
+    // setCount(count + 1);
+    const newTask:Task = {
+      id: Math.random(), //count, 
+      title: newTaskTitle, 
+      isComplete: false
+    }
+
+    setTasks(oldTasks => [...oldTasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const editedTask = tasks.map( task => task.id == id ? {...task, isComplete: !task.isComplete} : task);
+    setTasks(editedTask);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTask = tasks.filter( task => task.id !== id );
+    setTasks(filteredTask);
   }
 
   return (
